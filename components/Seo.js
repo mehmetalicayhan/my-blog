7b678109fdd61ca5adec7c6be1a9787e020bed3b
Mehmet Ali Çayhan <mehmetalicayhan@Mehmet-MacBook-Pro.local>
2020-03-11 16:32:54 +0300

    feat : seo test

---
+++ components/Seo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Seo() {
+  const site = {
+    title: 'Mehmet Ali Çayhan ',
+    description: 'Mehmet Ali Çayhan personal Blog site .3rd year Computer engineering student at Sakarya University',
+    url: 'https://mehmetalicayhan.com',
+  }
+
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+
+      <title>{site.title}</title>
+      <meta name="description" content={site.description} />
+
+
+      <meta property="og:type" content="personal.personal" />
+      <meta property="og:title" content={site.title} />
+      <meta property="og:url" content={site.url} />
+
+    </Head>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import App from "next/app";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Providers from "../components/theming/Providers";
-
+import Seo from "../components/Seo"
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <Providers>
+        <Seo/>
         <Component {...pageProps} />
       </Providers>
     );
