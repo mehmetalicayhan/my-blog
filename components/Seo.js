d8b8361a7be8b0cd084a2b99160298ee75abd301
Mehmet Ali Çayhan <mehmetalicayhan@Mehmet-MacBook-Pro.local>
2020-03-11 16:50:55 +0300

    chore : seo

--- components/Seo.js
+++ components/Seo.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
 export default function Seo() {
   const site = {
-    title: 'Mehmet Ali Çayhan - Personal Blog Site',
-    description: 'Mehmet Ali Çayhan personal Blog site .3rd year Computer engineering student at Sakarya University',
-    url: 'https://mehmetalicayhan.com',
-  }
+    title: "Mehmet Ali Çayhan - Personal Blog Site",
+    description:
+      "Mehmet Ali Çayhan personal Blog site .3rd year Computer engineering student at Sakarya University",
+    url: "https://mehmetalicayhan.com"
+  };
 
   return (
     <Head>
@@ -19,11 +20,10 @@ export default function Seo() {
       <title>{site.title}</title>
       <meta name="description" content={site.description} />
 
-
       <meta property="og:type" content="personal.personal" />
       <meta property="og:title" content={site.title} />
+      <meta property="og:description" content={site.description} />
       <meta property="og:url" content={site.url} />
-
     </Head>
-  )
+  );
 }
